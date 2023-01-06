790e334b78017a13430177bfb2c0aa225ab64985
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-01-06 17:07:46 -0500

    Data additions Added 2 new objects to the booksData array in books.js

--- data/books.js
+++ data/books.js
@@ -1,22 +1,32 @@
-export const booksData =[
+export const booksData = [
     {
-        title: '16 Accords of Madness, v. VI',
-        author: 'Anonymous',
+        title: "16 Accords of Madness, v. VI",
+        author: "Anonymous",
         isSkillBook: false
     },
     {
-        title: '16 Accords of Madness, v. IX',
-        author: 'Anonymous',
+        title: "16 Accords of Madness, v. IX",
+        author: "Anonymous",
         isSkillBook: false
     },
     {
-        title: '16 Accords of Madness, v. XII',
-        author: 'Anonymous',
+        title: "16 Accords of Madness, v. XII",
+        author: "Anonymous",
         isSkillBook: false
     },
     {
-        title: '2920, Morning Star, v1',
-        author: 'Carlovac Townway',
+        title: "2920, Morning Star, v1",
+        author: "Carlovac Townway",
         isSkillBook: true
+    },
+    {
+        title: "2920, Sun's Dawn, v1",
+        author: "Carlovac Townway",
+        isSkillBook: true
+    },
+    {
+        title: "An Accounting of the Scrolls",
+        author: "Quintus Nerevelus",
+        isSkillBook: false
     },
 ]
--- index.js
+++ index.js
@@ -2,29 +2,34 @@ import { booksData } from '/data/books.js'
 
 const bookContainer = document.getElementById('book-container')
 
+//function to iterate over our booksData array and return us an array of Books as objects
 function getBooksArray(books) {
     const booksArray = []
     for (let book of books){
         booksArray.push(book)
     }
-    //books.forEach(function(book) {
-        //booksArray.push(book)
-    //})
+    
     return booksArray
 }
 
+
+//takes in our generated booksArray and renders them to the page as HTML
 function renderBooks(books){
     let booksList = ``
     const skyrimBooks = getBooksArray(books)
 
     for (let book of skyrimBooks) {
-        booksList += `
+        booksList +=
+        `
         <div>
             <p class="book-title>${book.title}</p>
         </div>
         `
-    }
+   }
     bookContainer.innerHTML = booksList
+
+    console.log(skyrimBooks)
+    console.log(booksList)
 }
 
 renderBooks(booksData)
