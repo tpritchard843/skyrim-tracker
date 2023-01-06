cdc1967fcd93c1ff2dbf9c9d489a86c2721b3bb6
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-01-06 17:50:58 -0500

    PSA: check your HTML syntax
    Added a " after the class "book-title" on our index.js HTML.
    This fixed the javascript and the HTML output is now correct.
    
    Let this be a reminder to carefully write my code.

--- index.js
+++ index.js
@@ -2,34 +2,24 @@ import { booksData } from '/data/books.js'
 
 const bookContainer = document.getElementById('book-container')
 
-//function to iterate over our booksData array and return us an array of Books as objects
-function getBooksArray(books) {
-    const booksArray = []
-    for (let book of books){
-        booksArray.push(book)
-    }
-    
-    return booksArray
-}
-
 
 //takes in our generated booksArray and renders them to the page as HTML
 function renderBooks(books){
     let booksList = ``
-    const skyrimBooks = getBooksArray(books)
 
-    for (let book of skyrimBooks) {
+    for (let book of books){
         booksList +=
         `
         <div>
-            <p class="book-title>${book.title}</p>
+            <p class="book-title">${book.title}</p>
         </div>
         `
+        bookContainer.innerHTML = booksList
+        console.log(booksList)
+    }
+
+    console.log(booksData)
    }
-    bookContainer.innerHTML = booksList
 
-    console.log(skyrimBooks)
-    console.log(booksList)
-}
 
-renderBooks(booksData)
+window.addEventListener('DOMContentLoaded', renderBooks(booksData))
